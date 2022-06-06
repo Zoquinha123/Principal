c417c8d48378ff9784c8a6ae0afef25011695dd4
Zoquinha123 <gilson.morais@fatec.sp.gov.br>
2022-06-06 18:07:18 +0000

    gravando

---
+++ Numeros.java
@@ -0,0 +1,12 @@
+public class Numeros{
+
+    public static void main(String) args) {
+        int n = 38;
+
+        System.out.println("Todos os numeros de 38 a 1000. ");
+        for (int i = 38; i < 1001; 1++) {
+            System.out.println(i);
+
+        }
+    }
+}
